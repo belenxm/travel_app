3e74fbebc0ef8de8f5149350cff5a52708d56ff7
fer.belen <belenxmorales@gmail.com>
2023-11-02 11:24:14 -0300

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import GetApp from "@/components/GetApp";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
-   <div></div>
+   <div>
+    <Hero />
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp />
+   </div>
   )
 }
